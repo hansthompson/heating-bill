95ef51ddbc778524a682701e6f2e23ce772839ed
Charles Hans Thompson <hans.thompson1@gmail.com>
2015-01-13 13:12:29 -0900

    syntax

--- analysis.R
+++ analysis.R
@@ -9,7 +9,8 @@ gas <- cbind(gas, date)
 electric <- read.csv("electricity_usage.csv")
 electric$Read.Date <- mdy(electric$Read.Date)
 
-ggplot(data = electric, aes(x = mean_temp, y = use / days, color = energy_remodel))+ geom_point(size = 3) + geom_smooth(method = "lm", se = FALSE) + 
+ggplot(data = electric, aes(x = mean_temp, y = use / days, color = energy_remodel)) + 
+    geom_point(size = 3) + geom_smooth(method = "lm", se = FALSE) + 
     labs(x= "Mean Temperature", 
          y = "Mean Ccf", 
          title = "Average energy consumtion over outside temperature") 
@@ -17,18 +18,20 @@ ggplot(data = electric, aes(x = mean_temp, y = use / days, color = energy_remode
 electric_rate <- 0.149994
 gas_rate <- 0.76891 + 0.1128
 
-ggplot(data = electric, aes(x = Read.Date, y = electric_rate * KWH)) + geom_bar(stat = "identity") +
+ggplot(data = electric, aes(x = Read.Date, y = electric_rate * KWH)) + 
+    geom_bar(stat = "identity") +
     labs(x= "Time", 
          y = "Cost for Usage - $", 
          title = "Electric Cost At $0.14449/KWH") 
 
-
-ggplot(data = gas, aes(x = mean_temp, y = use / days, color = energy_remodel))+ geom_point(size = 3) + geom_smooth(method = "lm", se = FALSE) + 
+ggplot(data = gas, aes(x = mean_temp, y = use / days, color = energy_remodel)) + 
+    geom_point(size = 3) + geom_smooth(method = "lm", se = FALSE) + 
     labs(x= "Mean Temperature", 
          y = "Mean Ccf", 
          title = "Average energy consumtion over outside temperature") 
 
-ggplot(data = gas, aes(x = date, y = gas_rate * (use)), fill = owner) + geom_bar(stat= "identity") +
+ggplot(data = gas, aes(x = date, y = gas_rate * (use)), fill = owner) + 
+    geom_bar(stat= "identity") +
     labs(x= "Time", 
          y = "Cost for Usage - $", 
          title = "Gas Cost at $0.88171/ccf") 
